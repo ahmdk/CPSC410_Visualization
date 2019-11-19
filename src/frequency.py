dc1b3695553dbd7e2c8ab953c80e4b57eab4a664
jibril101 <mohsinak49@gmail.com>
2019-11-19 11:07:33 -0800

    frequency of calls class initial

--- src/frequency.py
+++ src/frequency.py
@@ -1,2 +1,19 @@
-def count():
-    
+import inspect
+
+
+class Frequency:
+    """Logs the frequency of calls to particular functions in a given source code"""
+
+    def __init__(self):
+        self.function_frequency = {}
+
+    def count(self):
+        "check which function you re being called from and increment that function's call frequency"
+        caller = inspect.stack()[2][3]
+        self.function_frequency[caller] += 1
+
+
+if __name__ == "__main__":
+    f = Frequency()
+    f.count()
+
